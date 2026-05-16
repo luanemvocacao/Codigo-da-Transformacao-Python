64ec85631c4503013dbf10863bd716a4ecb0dc28
luanemvocacao <seijiyugue@gmail.com>
2026-05-16 15:38:49 -0300

    Create utilidade.py

---
+++ utilidade.py
@@ -0,0 +1,7 @@
+# main.py
+import utilidades  # importando nosso módulo
+
+# Usando as funções do módulo
+print("Soma: ", utilidades.soma(5, 3))
+print("Subtração: ", utilidades.subtracao(10, 4))
+print("Potência: ", utilidades.potencia(2, 3))
